af10934ccb8c65d3bf6d41cf2d63365c3186028a
jongho <jj0101065@gmail.com>
2022-05-12 02:04:56 +0900

    create

--- src/main/java/com/example/toywebservice/controller/TodoController.java
+++ src/main/java/com/example/toywebservice/controller/TodoController.java
@@ -1,31 +1,49 @@
 package com.example.toywebservice.controller;
 
 import com.example.toywebservice.dto.ResponseDTO;
+import com.example.toywebservice.dto.TodoDTO;
+import com.example.toywebservice.model.TodoEntity;
 import com.example.toywebservice.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("todo")
+@RequestMapping("/todo")
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoService service;
 
-    @GetMapping("/test")
-    public ResponseEntity<?> testTodo() {
-        String str = service.testS();
-        List<String> list = new ArrayList<>();
-        list.add(str);
-        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-        return ResponseEntity.ok().body(response);
-    }
 
+    @PostMapping("")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            entity.setId(null);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
 }
--- src/main/java/com/example/toywebservice/service/TodoService.java
+++ src/main/java/com/example/toywebservice/service/TodoService.java
@@ -3,19 +3,33 @@ package com.example.toywebservice.service;
 import com.example.toywebservice.model.TodoEntity;
 import com.example.toywebservice.persistence.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TodoService {
 
     private final TodoRepository repository;
 
-    public String testS() {
-        TodoEntity entity = TodoEntity.builder().title("first").build();
+    public List<TodoEntity> create(final TodoEntity entity) {
+        validate(entity);
         repository.save(entity);
-        TodoEntity saved = repository.findById(entity.getId()).get();
-        return saved.getTitle();
+
+        return repository.findByUserId(entity.getUserId());
     }
 
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("error");
+            throw new RuntimeException("unknown user");
+        }
+        if (entity.getUserId() == null) {
+            log.warn("error getUserId");
+            throw new RuntimeException("unknown getUserId");
+        }
+    }
 }
